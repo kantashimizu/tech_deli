abf633bfbfd4c0ff221d50c73e57c5162c9ce970
tamutomo223 <57092560+tamutomo223@users.noreply.github.com>
2019-11-30 12:55:48 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "drinks#index"
-  resources :orders, only: [:new,:create]
+  get "/orders/new/:id" ,to: "orders#new", as: "new_order"
+  post "/orders/:id", to:"orders#create", as: "order" 
 end
